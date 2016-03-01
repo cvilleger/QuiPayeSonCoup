4fd1e90615247ee20806478bc1dd7ee664843fb2
margot <m.hanszechuen@gmail.com>
2016-03-01 14:48:41 +0100

    fixtures pour room et invitation

---
+++ src/AppBundle/DataFixtures/ORM/LoadInvitationData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Invitation;
+
+class LoadInvitationData extends AbstractFixture implements OrderedFixtureInterface{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager){
+        $invitation1 = new Invitation();
+        $invitation1->setUser($this->getReference('user'));
+        $invitation1->setRoom($this->getReference('room'));
+        $invitation1->setDate(new \DateTime());
+
+        $invitation2 = new Invitation();
+        $invitation2->setUser($this->getReference('user2'));
+        $invitation2->setRoom($this->getReference('room'));
+        $invitation2->setDate(new \DateTime());
+
+        $manager->persist($invitation1);
+        $manager->persist($invitation2);
+        $manager->flush();
+    }
+
+    public function getOrder(){
+        return 2;
+    }
+}
---
+++ src/AppBundle/DataFixtures/ORM/LoadRoomData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Room;
+
+class LoadRoomData extends AbstractFixture implements OrderedFixtureInterface{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager){
+        $room = new Room();
+        $room->setAdministrator($this->getReference('user'));
+        $room->setName('Ecole');
+        $room->setSlug('ecole');
+        $room->setIsActivated(true);
+        $room->setDateStart(new \DateTime());
+
+        $room2 = new Room();
+        $room2->setAdministrator($this->getReference('user'));
+        $room2->setName('Travail');
+        $room2->setSlug('travail');
+        $room2->setIsActivated(true);
+        $room2->setDateStart(new \DateTime());
+
+        $manager->persist($room);
+        $manager->persist($room2);
+        $manager->flush();
+
+        $this->addReference('room', $room);
+    }
+
+    public function getOrder(){
+        return 1;
+    }
+}
