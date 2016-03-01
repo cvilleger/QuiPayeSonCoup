cf77c94c84d15e3387cf16952550027258918949
margot <m.hanszechuen@gmail.com>
2016-03-01 12:01:00 +0100

    relation room-user

--- src/AppBundle/Entity/Room.php
+++ src/AppBundle/Entity/Room.php
@@ -63,6 +63,18 @@ class Room
      */
     private $photo;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\User", inversedBy="rooms", cascade={"remove"})
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    protected $administrateur;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="UserBundle\Entity\User", inversedBy="rooms")
+     * @ORM\JoinTable(name="user_room")
+     */
+    protected $users;
+
 
     /**
      * Get id
@@ -210,5 +222,68 @@ class Room
     public function getPhoto()
     {
         return $this->photo;
+    }
+
+    /**
+     * Set administrateur
+     *
+     * @param \UserBundle\Entity\User $administrateur
+     * @return Room
+     */
+    public function setAdministrateur(\UserBundle\Entity\User $administrateur = null)
+    {
+        $this->administrateur = $administrateur;
+
+        return $this;
+    }
+
+    /**
+     * Get administrateur
+     *
+     * @return \UserBundle\Entity\User 
+     */
+    public function getAdministrateur()
+    {
+        return $this->administrateur;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add users
+     *
+     * @param \UserBundle\Entity\User $users
+     * @return Room
+     */
+    public function addUser(\UserBundle\Entity\User $users)
+    {
+        $this->users[] = $users;
+
+        return $this;
+    }
+
+    /**
+     * Remove users
+     *
+     * @param \UserBundle\Entity\User $users
+     */
+    public function removeUser(\UserBundle\Entity\User $users)
+    {
+        $this->users->removeElement($users);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getUsers()
+    {
+        return $this->users;
     }
 }
