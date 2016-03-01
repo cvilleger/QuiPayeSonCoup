bd1bc8d9a7e98274185308c31adeae0f044519b1
Christophe Villeger <christophe.villeger@alcatel-lucent.com>
2016-03-01 14:54:59 +0100

    Create Room Service

---
+++ src/AppBundle/Service/RoomService.php
@@ -0,0 +1,64 @@
+<?php
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Room;
+use Doctrine\ORM\EntityManager;
+use UserBundle\Entity\User;
+
+class RoomService {
+
+    /* @var EntityManager */
+    protected $em;
+
+    protected $roomRepository;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    function __construct(EntityManager $entityManager){
+        $this->em = $entityManager;
+        $this->roomRepository = $this->em->getRepository('AppBundle:Room');
+    }
+
+    /**
+     * Get all rooms for a user
+     * @param User $user
+     */
+    function getRoomsByUser(User $user){
+        $this->roomRepository->findByUser($user);
+    }
+
+    /**
+     * Search for a room through name
+     * Return an array of room
+     * @param String $term
+     * @return array
+     */
+    public function searchRoomByName($term){
+        return $this->roomRepository->createQueryBuilder('r')
+            ->where('r.name LIKE :term')
+            ->setParameter('term', '%'.$term.'%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * Remove a room from database
+     * @param Room $room
+     */
+    function remove(Room $room){
+        $this->em->remove($room);
+        $this->em->flush($room);
+    }
+
+    /**
+     * Save a room into database
+     * @param Room $room
+     */
+    function save(Room $room){
+        $this->em->persist($room);
+        $this->em->flush($room);
+    }
+
+
+}
