1d0bd94a984c0f1a3ed709a537eefde9927ef883
baptistelinel <baptiste.linel@gmail.com>
2016-03-02 18:26:45 +0100

    New action in RoomController : edit room by slug

---
+++ src/AppBundle/Controller/RoomController.php
@@ -0,0 +1,61 @@
+<?php
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Room;
+use AppBundle\Form\RoomType;
+use AppBundle\Service\RoomService;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class RoomController extends Controller
+{
+    /** @var  Request $request */
+    private $request;
+
+    /** @var  RoomService $roomService */
+    private $roomService;
+
+    public function preExecute(Request $request){
+        $this->request = $request;
+    }
+
+    /**
+     * Edit or add a Room
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(){
+        $slug = $this->request->get('slug');
+
+        // Add a new Room
+        if($slug == false || empty($slug)) {
+            $room = new Room();
+        }
+        // Edit a room
+        else{
+            $room = $this->roomService->getRoomBySlug($slug);
+
+            // No room found by given slug
+            if(empty($room)){
+                $room = new Room();
+            }
+        }
+
+        $form = $this->createForm(new RoomType(), $room);
+        
+        if($this->request->isMethod('POST')){
+            $form->handleRequest($this->request);
+
+            if($form->isValid()){
+                $this->roomService->save($room);
+
+                return $this->redirect($this->generateUrl('room_view', array(
+                    'slug' => $room->getSlug()
+                )));
+            }
+        }
+
+        return $this->render('AppBundle:Room:edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
